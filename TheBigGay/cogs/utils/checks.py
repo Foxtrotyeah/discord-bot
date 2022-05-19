8707059e0b6308cf09c97ca3023a2074839088fa
foxtrotyeah <foxtrottrick@gmail.com>
2022-05-19 16:16:34 -0600

    has_funds is no longer a decorator

--- TheBigGay/cogs/utils/checks.py
+++ TheBigGay/cogs/utils/checks.py
@@ -4,14 +4,11 @@ from discord.ext import commands
 from . import mysql
 
 
-def has_funds(amt: int, member: discord.Member = None):
-    def pred(ctx) -> bool:            
-        if mysql.get_balance(member if member else ctx.author) >= amt:
-            return True
-        else:
-            return False
-
-    return commands.check(pred)
+def has_funds(member: discord.Member, amt: int):
+    if mysql.get_balance(member) >= amt:
+        return True
+    else:
+        raise commands.CommandError(f"Insufficient funds: {member.mention}")
 
 
 def is_gambling_category():
