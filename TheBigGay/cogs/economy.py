b2acc3b25549ce2d2cbace5a2348a65fc0a6c11f
foxtrotyeah <foxtrottrick@gmail.com>
2022-05-26 15:53:24 -0600

    Moved error handling to bot.py Removed individual cooldowns .economy now returns top 5 .donate no longer allows for donating to oneself Updated command briefs

--- TheBigGay/cogs/economy.py
+++ TheBigGay/cogs/economy.py
@@ -10,14 +10,6 @@ class Economy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError): 
-        if isinstance(error, checks.WrongChannel):                
-            await ctx.message.delete()
-            return await ctx.send(f"{ctx.author.mention} {error}", delete_after=10)
-
-        else:
-            return await ctx.send(f"{ctx.author.mention} {error}")
-
     @commands.Cog.listener()
     async def on_ready(self):
         guilds = self.bot.guilds
@@ -26,44 +18,41 @@ class Economy(commands.Cog):
             mysql.initialize_guild(guild)
 
     @commands.command(brief="Get your current balance.", description="Retrieve your current balance in gaybucks.")
-    @commands.cooldown(1, 15, commands.BucketType.user)
     async def balance(self, ctx: commands.Context):
         result = mysql.get_balance(ctx.author)
         await ctx.send(f"{ctx.author.mention}, your balance is {result} gaybucks.")
 
-    @commands.command(brief="Running low on funds? Use this once per day!",
+    @commands.command(brief="Poor? Use this once per day!",
                       description="You must have less than 50 gaybucks in your account to be eligible. "
                                   "You can also only receive a subsidy once per day.")
     @checks.is_gambling_category()
     @checks.check_subsidy()
-    @commands.cooldown(1, 60, commands.BucketType.user)
     async def subsidy(self, ctx: commands.Context):
         mysql.subsidize(ctx.author)
 
         await ctx.send(f"{ctx.author.mention}, 50 gaybucks have been added to your account, "
                        f"courtesy of your sugar daddy 😉 (You now have {mysql.get_balance(ctx.author)} GB)")
 
-    # TODO only show top 10 or something. Too long
     @commands.command(brief="Check the current economy standings.",
                       description="Shows each member's current balance in gaybucks.")
-    @commands.cooldown(1, 60, commands.BucketType.guild)
     async def economy(self, ctx: commands.Context):
         economy = mysql.get_economy(ctx.guild)
         sorted_economy = sorted(economy, key=lambda tup: tup[1], reverse=True)
 
-        embed = discord.Embed(title="Economy Standings", color=discord.Color.purple())
-        for row in sorted_economy:
+        embed = discord.Embed(title="Economy Top 5", color=discord.Color.purple())
+        for row in sorted_economy[:5]:
             member = ctx.guild.get_member(int(row[0]))
             balance = row[1]
             embed.add_field(name=f"{member.name}", value=f"{balance} GB", inline=False)
 
         await ctx.send(embed=embed)
 
-    @commands.command(brief="Donate gaybucks to another member of the server.",
+    @commands.command(brief="Donate gaybucks to another member.",
                       description="Donate gaybucks to another member of the server.")
     @checks.is_gambling_category()
-    @commands.cooldown(1, 60, commands.BucketType.guild)
     async def donate(self, ctx: commands.Context, member: discord.Member, amt: int):
+        if member.id == ctx.author.id:
+            return await ctx.send(f"{ctx.author.mention} Wow, how generous...")
 
         checks.is_valid_bet(ctx.author, amt)
 
