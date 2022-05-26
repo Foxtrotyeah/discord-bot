4b7424a5723a7fca1facc9660cc7d51ab805cd96
foxtrotyeah <foxtrottrick@gmail.com>
2022-05-26 15:55:02 -0600

    Fixed incorrect path for assets Removed individual cooldowns Updated command briefs

--- TheBigGay/cogs/misc.py
+++ TheBigGay/cogs/misc.py
@@ -19,37 +19,32 @@ class Miscellaneous(commands.Cog):
     @commands.command(brief="The bot's current latency.",
                       description="Gives the latency between the bot and server in ms.",
                       hidden=True)
-    @commands.cooldown(1, 5, commands.BucketType.user)
     async def ping(self, ctx: commands.Context):
         await ctx.send(f"Pong! My latency right now is {round(self.bot.latency * 1000)}ms")
 
     @commands.command(brief="Says hi, but gayer", description="Says hi, but gayer.", hidden=True)
-    @commands.cooldown(1, 5, commands.BucketType.user)
     async def hello(self, ctx: commands.Context):
         await ctx.send(f"{ctx.author.mention} Heyyy ;)")
 
     @commands.command(brief="Roasts a user.", description="Roast a user with a preset insult from The Big Gay")
-    @commands.cooldown(1, 15, commands.BucketType.user)
     async def roast(self, ctx: commands.Context, member: discord.Member):
         if member.bot:
             return await ctx.send(f"{ctx.author.mention} No one roasts me, twat.")
         if member.status == discord.Status.offline:
-            await ctx.send(f"Damn, you're trying to roast someone behind their back! {member.mention} isn't even online.")
+            return await ctx.send(f"Damn, you're trying to roast someone behind their back! {member.mention} isn't even online.")
 
-        with open("discord/assets/text.json", encoding="utf8", errors="ignore") as file:
+        with open("assets/text.json", encoding="utf8", errors="ignore") as file:
             selected = random.choice(json.load(file)["roasts"]).format(member.mention)
         await ctx.send(selected)
 
     @commands.command(brief="Pastes a hot emojipasta.", description="Pastes only the greatest from r/emojipastas")
-    @commands.cooldown(1, 60, commands.BucketType.user)
     async def emojipasta(self, ctx: commands.Context):
-        with open("discord/assets/text.json", encoding="utf8", errors="ignore") as file:
+        with open("assets/text.json", encoding="utf8", errors="ignore") as file:
             selected = random.choice(json.load(file)["emojipastas"])
         await ctx.send(selected)
 
     @commands.command(brief="Vibecheck a user or the whole server.",
                       description="If no user is specified, the whole server will be vibechecked.")
-    @commands.cooldown(1, 15, commands.BucketType.user)
     async def vibecheck(self, ctx: commands.Context, member: discord.Member = None):
         if member:
             if member.bot:
@@ -86,15 +81,13 @@ class Miscellaneous(commands.Cog):
             await ctx.send(vibe)
 
     @commands.command(brief="A 50/50 virtual coin toss.", description="Completely random 50/50 decision")
-    @commands.cooldown(1, 5, commands.BucketType.user)
     async def coinflip(self, ctx: commands.Context):
         coin = ["Heads!", "Tails!"]
         await ctx.send(random.choice(coin))
 
-    @commands.command(brief="I will select from a list of options at random.",
+    @commands.command(brief="I will make a decision for you.",
                       description="Can't decide for yourself? Let computers think for you. "
                                   "Give me a list of options separated by commas.")
-    @commands.cooldown(1, 15, commands.BucketType.user)
     async def choose(self, ctx: commands.Context, *, inputs: str):
         choices = str(inputs).split(", ")
         return await ctx.send(f"{ctx.author.mention} I choose **{random.choice(choices)}**!")
@@ -104,7 +97,7 @@ class Miscellaneous(commands.Cog):
         timezone = pytz.timezone("US/Pacific")
         await ctx.send("PST time: {}".format(datetime.now(timezone)))
 
-    @commands.command(brief="Submit an anonymous request for a new idea or bug.",
+    @commands.command(brief="Send the owner a request or bug report.",
                       description="Send the owner of The Big Gay a request for a feature idea "
                                   "or a bug that you may have come across.")
     async def request(self, ctx: commands.Context, *, content: str):
