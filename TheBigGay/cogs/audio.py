ecc4eddd394a6479ba97eb576421f34b7d71c2b3
foxtrotyeah <foxtrottrick@gmail.com>
2022-05-26 15:52:07 -0600

    Removed individual cooldowns All soundbytes now use gaybucks

--- TheBigGay/cogs/audio.py
+++ TheBigGay/cogs/audio.py
@@ -4,6 +4,8 @@ import os
 import youtube_dl
 import asyncio
 
+from .utils import checks
+
 
 class Audio(commands.Cog, command_attrs=dict(hidden=True)):
     def __init__(self, bot):
@@ -13,6 +15,7 @@ class Audio(commands.Cog, command_attrs=dict(hidden=True)):
     async def audio(self, ctx: commands.Context):
         embed = discord.Embed(
             title="Audio Commands", 
+            description="All soundbytes cost 20 gaybucks to use.",
             color=discord.Color.blue()
         )
 
@@ -41,6 +44,8 @@ class Audio(commands.Cog, command_attrs=dict(hidden=True)):
     # todo Make this functional as a bot command. How to disconnect?? Don't know how to realize when audio is finished
     # TODO Add a check to see if bot is already connected to a voice channel
     async def play(self, ctx: commands.Context, url: str, name: str, channel: discord.VoiceChannel, wait: int = 60 * 5):
+        checks.is_valid_bet(ctx.author, 20)
+
         is_there = os.path.isfile(f"./tracks/{name}")
         if not is_there:
             ydl_opts = {
@@ -63,85 +68,71 @@ class Audio(commands.Cog, command_attrs=dict(hidden=True)):
         await voice.disconnect()
 
     @commands.command(brief="Soudbyte", description="What are you aiming at?")
-    @commands.cooldown(1, 30, commands.BucketType.category)
     async def aim(self, ctx: commands.Context):
         channel = ctx.guild.voice_channels[0]
         await self.play(ctx, url="https://youtu.be/VoOxzT1ngeM", name="aim.mp3", channel=channel, wait=5)
 
     @commands.command(brief="Soudbyte", description="It smells like bitch in here.")
-    @commands.cooldown(1, 30, commands.BucketType.category)
     async def bitch(self, ctx: commands.Context):
         channel = ctx.guild.voice_channels[0]
         await self.play(ctx, url="https://youtu.be/ppxmxVhkr7c", name="bitch.mp3", channel=channel, wait=5)
 
     @commands.command(brief="Soudbyte", description="Lord have mercy.")
-    @commands.cooldown(1, 30, commands.BucketType.category)
     async def bust(self, ctx: commands.Context):
         channel = ctx.guild.voice_channels[0]
         await self.play(ctx, url="https://youtu.be/jzge_j-_PME", name="bust.mp3", channel=channel, wait=5)
 
     @commands.command(brief="Soudbyte", description="You just have to say that you're fine.")
-    @commands.cooldown(1, 30, commands.BucketType.category)
     async def fine(self, ctx: commands.Context):
         channel = ctx.guild.voice_channels[0]
         await self.play(ctx, url="https://youtu.be/77sS5IuR0Gs", name="fine.mp3", channel=channel, wait=7)
 
     @commands.command(brief="Soudbyte", description="Plays the 'get your shit together' scene from Rick and Morty.")
-    @commands.cooldown(1, 30, commands.BucketType.category)
     async def morty(self, ctx: commands.Context):
         channel = ctx.guild.voice_channels[0]
         await self.play(ctx, url="https://youtu.be/UYKKQn3WXh0", name="morty.mp3", channel=channel, wait=21)
 
     @commands.command(brief="Soudbyte", description="Plays audio from the 'It's Wednesday my dudes' scream.")
-    @commands.cooldown(1, 30, commands.BucketType.category)
     async def nut(self, ctx: commands.Context):
         channel = ctx.guild.voice_channels[0]
         await self.play(ctx, url="https://youtu.be/dlRKA1nirrg", name="nut.mp3", channel=channel, wait=4)
 
     @commands.command(brief="Soudbyte", description="Plays audio from the death sound in Roblox.")
-    @commands.cooldown(1, 30, commands.BucketType.category)
     async def oof(self, ctx: commands.Context):
         channel = ctx.guild.voice_channels[0]
         await self.play(ctx, url="https://youtu.be/3w-2gUSus34", name="oof.mp3", channel=channel, wait=3)
 
     @commands.command(brief="Soudbyte", description="Plays TikTok sheesh sound.")
-    @commands.cooldown(1, 30, commands.BucketType.category)
     async def sheesh(self, ctx: commands.Context):
         channel = ctx.guild.voice_channels[0]
         await self.play(ctx, url="https://youtu.be/hRj1VyUsVjw", name="sheesh.mp3", channel=channel, wait=13)
 
     @commands.command(brief="Soudbyte", description="Plays audio from this dude having a real good time while cooking.")
-    @commands.cooldown(1, 30, commands.BucketType.category)
     async def sogood(self, ctx: commands.Context):
         channel = ctx.guild.voice_channels[0]
         await self.play(ctx, url="https://youtu.be/-FrpuPLYnvY", name="sogood.mp3", channel=channel, wait=17)
 
     @commands.command(brief="Soudbyte", description="Plays audio from the 'Damn, this n\*\*\*\* spitting' meme")
-    @commands.cooldown(1, 30, commands.BucketType.category)
     async def spitting(self, ctx: commands.Context):
         channel = ctx.guild.voice_channels[0]
         await self.play(ctx, url="https://youtu.be/xuLfu0z5_m0", name="spitting.mp3", channel=channel, wait=5)
 
     @commands.command(brief="Soudbyte", description="Plays audio from the spongebob heckler.")
-    @commands.cooldown(1, 30, commands.BucketType.category)
     async def stinks(self, ctx: commands.Context):
         channel = ctx.guild.voice_channels[0]
         await self.play(ctx, url="https://youtu.be/ppb8A5QqPj8", name="stinks.mp3", channel=channel, wait=4)
 
     @commands.command(brief="Soudbyte", description="Begone...")
-    @commands.cooldown(1, 30, commands.BucketType.category)
     async def thot(self, ctx: commands.Context):
         channel = ctx.guild.voice_channels[0]
         await self.play(ctx, url="https://youtu.be/tyrKeThaEJM", name="thot.mp3", channel=channel, wait=5)
 
     @commands.command(brief="Soudbyte", description="Uh oh stinky")
-    @commands.cooldown(1, 30, commands.BucketType.category)
     async def uhoh(self, ctx: commands.Context):
         channel = ctx.guild.voice_channels[0]
         await self.play(ctx, url="https://youtu.be/PJNBJAYFyKE", name="uhoh.mp3", channel=channel, wait=5)
 
     @commands.command(brief="Soudbyte", description="Plays audio from a guy hitting a real nice yeet.")
-    @commands.cooldown(1, 30, commands.BucketType.category)
     async def yeet(self, ctx: commands.Context):
         channel = ctx.guild.voice_channels[0]
         await self.play(ctx, url="https://youtu.be/won6qew1da4", name="yeet.mp3", channel=channel, wait=7)
