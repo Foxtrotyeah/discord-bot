967911f28a6e08c379d919dad0de1c36d33a334e
foxtrotyeah <foxtrottrick@gmail.com>
2023-03-21 10:46:31 -0600

    soundbytes are no longer needed on the bot core audio functions moved to utils instead of being a cog

---
+++ TheBigGay/cogs/utils/audio.py
@@ -0,0 +1,48 @@
+import discord
+from discord.ext import commands
+import os
+import youtube_dl
+import asyncio
+
+
+async def before_play(ctx: commands.Context):
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+    else:
+        channel = ctx.guild.voice_channels[0]
+
+    return channel
+
+    
+def after_play(error: Exception):
+    if error:
+        raise commands.CommandError(error)
+
+
+async def play(bot: commands.Bot, url: str, name: str, ctx: commands.Context = None, channel: discord.VoiceChannel = None, wait: int = 60 * 5): 
+    if ctx and not channel:
+        channel = await before_play(ctx)
+
+    if not os.path.isfile(f"./tracks/{name}"):
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': f'./tracks/{name}',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192'
+            }],
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+
+    voice = discord.utils.get(bot.voice_clients, guild=channel.guild)
+    if voice and voice.is_playing():
+        raise commands.CommandError("I'm already playing something. Wait a sec")
+
+    voice_client = await channel.connect()
+
+    voice_client.play(discord.FFmpegPCMAudio(source=f"./tracks/{name}"), after=after_play)
+
+    await asyncio.sleep(wait)
+    await voice_client.disconnect()
