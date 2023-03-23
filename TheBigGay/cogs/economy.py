22b205bcb00dad66a0bbadd29099668a0bb30d7e
foxtrotyeah <foxtrottrick@gmail.com>
2023-03-23 17:44:01 -0600

    converting commands to app commands

--- TheBigGay/cogs/economy.py
+++ TheBigGay/cogs/economy.py
@@ -1,10 +1,14 @@
 import discord
+from discord import app_commands
 from discord.ext import commands, tasks
-from datetime import datetime, timedelta
+import datetime
 from dateutil.relativedelta import relativedelta
 
 from .utils import mysql
 from .utils import checks
+
+
+time = datetime.time(hour=21, tzinfo=mysql.timezone)
 
 
 class Economy(commands.Cog):
@@ -19,7 +23,7 @@ class Economy(commands.Cog):
 
         self.check_lottery.start()
 
-    @tasks.loop(hours=24)
+    @tasks.loop(time=time)
     async def check_lottery(self):
         for guild in self.bot.guilds:
             # Check for lottery events
@@ -29,7 +33,7 @@ class Economy(commands.Cog):
                 drawing_event = [await self.create_lottery_event(guild)]
 
             # Lottery is held on the last day of every month. Heroku reloads daily, so this will be checked daily.
-            if drawing_event[0].start_time.date() == datetime.now().date():
+            if drawing_event[0].start_time.date() == datetime.datetime.now().date():
                 channels = [item[1] for item in guild.by_category() if item[0].name == "Gambling"][0]
                 main_hall = [channel for channel in channels if channel.name == "main-hall"][0]
 
@@ -43,11 +47,11 @@ class Economy(commands.Cog):
                 await drawing_event[0].delete()
                 await self.create_lottery_event(guild)
 
-    async def create_lottery_event(self, guild: discord.Guilde) -> discord.ScheduledEvent:
-        # Create the date for the first of next month at 8:00p
-        now = datetime.now(mysql.timezone)
-        start_time = datetime(now.year, now.month, 1, 20) + relativedelta(months=1)
-        end_time = start_time + timedelta(minutes=15)
+    async def create_lottery_event(self, guild: discord.Guild) -> discord.ScheduledEvent:
+        # Create the date for the first of next month at 8:00p Pacific. discord.py incorrectly inputs 8pm MST as 9pm MST.
+        now = datetime.datetime.now(mysql.timezone)
+        start_time = datetime.datetime(now.year, now.month, 1, 20, tzinfo=mysql.timezone) + relativedelta(months=1)
+        end_time = start_time + datetime.timedelta(minutes=15)
 
         description = (
             "A random lottery ticket will be pulled to choose the winner of the entire jackpot! "
@@ -55,68 +59,62 @@ class Economy(commands.Cog):
         )
         return await guild.create_scheduled_event(name="Lottery Drawing", description=description, start_time=start_time, end_time=end_time, entity_type=discord.EntityType.external, location="main-hall")
 
-    @commands.command(brief="Get your current balance.", description="Retrieve your current balance in gaybucks.")
-    async def wallet(self, ctx: commands.Context):
-        result = mysql.get_wallet(ctx.author)
-        await ctx.send(f"{ctx.author.mention}, your balance is **{result[0]}** gaybucks and you have **{result[1]}** lottery tickets.")
+    @app_commands.command(description="Get your current balance and lottery tickets")
+    async def wallet(self, interaction: discord.Interaction):
+        result = mysql.get_wallet(interaction.user)
+        await interaction.response.send_message(f"Your balance is **{result[0]}** gaybucks and you have **{result[1]}** lottery ticket(s).", ephemeral=True)
 
-    @commands.command(brief="Poor? Use this once per day!",
-                      description="You must have less than 50 gaybucks in your account to be eligible. "
-                                  "You can also only receive a subsidy once per day.")
+    @app_commands.command(description="Receive your daily 50GB subsidy (if you're poor)")
     @checks.check_subsidy()
-    @checks.is_gambling_category()
-    async def subsidy(self, ctx: commands.Context):
-        balance = mysql.subsidize(ctx.author)
+    async def subsidy(self, interaction: discord.Interaction):
+        balance = mysql.subsidize(interaction.user.mention)
 
-        await ctx.send(f"{ctx.author.mention}, 50 gaybucks have been added to your account, "
-                       f"courtesy of your sugar daddy 😉 (You now have **{balance} GB**)")
+        await interaction.response.send_message(f"{interaction.user.mention}, 50 gaybucks have been added to your account, "
+                       f"courtesy of your sugar daddy 😉 (You now have **{balance}GB**)")
 
-    @commands.command(brief="Check the current economy standings.",
-                      description="Shows each member's current balance in gaybucks.")
-    async def economy(self, ctx: commands.Context):
-        economy = mysql.get_economy(self.bot, ctx.guild)
+    @app_commands.command(description="Shows each member's current GB balance")
+    async def economy(self, interaction: discord.Interaction):
+        economy = mysql.get_economy(self.bot, interaction.guild)
         sorted_economy = sorted(economy, key=lambda tup: tup[1], reverse=True)
 
         embed = discord.Embed(title="Economy Top 5", color=discord.Color.purple())
         for row in sorted_economy[:5]:
-            member = ctx.guild.get_member(int(row[0]))
+            member = interaction.guild.get_member(int(row[0]))
             balance = row[1]
-            embed.add_field(name=f"{member.name}", value=f"{balance} GB", inline=False)
+            embed.add_field(name=f"{member.name}", value=f"{balance}GB", inline=False)
 
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.command(brief="Donate gaybucks to another member.",
-                      description="Donate gaybucks to another member of the server.")
-    @checks.is_gambling_category()
-    async def donate(self, ctx: commands.Context, member: discord.Member, amt: int):
-        if member.id == ctx.author.id:
-            return await ctx.send(f"{ctx.author.mention} Wow, how generous...")
+    @app_commands.command(description="Donate gaybucks to another member of the server")
+    @app_commands.describe(member="the member to donate to", amount="amount of GB to donate")
+    async def donate(self, interaction: discord.Interaction, member: discord.Member, amount: int):
+        if member.id == interaction.user.id:
+            return await interaction.response.send_message(f"{interaction.user.mention} Wow, how generous...?")
 
-        checks.is_valid_bet(ctx, ctx.author, amt)
+        checks.is_valid_bet(interaction.channel, interaction.user, amount)
 
-        mysql.update_balance(ctx.author, -amt)
-        balance = mysql.update_balance(member, amt)
-        await ctx.send(f"{ctx.author.mention} has just donated {amt} GB to {member.mention}! They now have {balance} GB.")
+        mysql.update_balance(interaction.user, -amount)
+        balance = mysql.update_balance(interaction.user, amount)
+        await interaction.response.send_message(f"{interaction.user.mention} has just donated {amount}GB to {member.mention}! They now have {balance}GB.")
 
-    @commands.command(brief="Buy lottery tickets for 50 GB each.", description="Lottery tickets get you a chance to win the monthly lottery. 50 GB each.")
-    @checks.is_gambling_category()
-    async def ticket(self, ctx:commands.Context, amt: int = 1):
+    @app_commands.command(description="Buy lottery tickets for 50GB each")
+    @app_commands.describe(amount="amount of lottery tickets to buy")
+    async def ticket(self, interaction: discord.Interaction, amount: int = 1):
         ticket_price = 50
-        checks.is_valid_bet(ctx, ctx.author, amt * ticket_price)
+        checks.is_valid_bet(interaction.channel, interaction.user, amount * ticket_price)
 
-        tickets = mysql.buy_ticket(self.bot, ctx.author, amt)
+        tickets = mysql.buy_ticket(self.bot, interaction.user, amount)
 
-        await ctx.send(f"{ctx.author.mention}, you now have **{tickets}** lottery tickets.")
+        await interaction.response.send_message(f"{interaction.user.mention}, you now have **{tickets}** lottery tickets.")
 
-    @commands.command(brief="Check the current jackpot.", description="Shows the current lottery jackpot.")
-    @checks.is_gambling_category()
-    async def lottery(self, ctx: commands.Context):
-        result = mysql.get_lottery(self.bot, ctx.guild)
+    @app_commands.command(description="Shows the current lottery jackpot.")
+    async def lottery(self, interaction: discord.Interaction):
+        result = mysql.get_lottery(self.bot, interaction.guild)
 
-        description = f"The current lottery jackpot is **{result} gaybucks**. Buy your tickets with **.ticket** before the drawing at the end of the month!"
+        description = f"The current lottery jackpot is **{result} gaybucks**. Buy your tickets with **/ticket** before the drawing at the end of the month!"
         
         embed = discord.Embed(title="Lottery", description=description, color=discord.Color.gold())
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
 
 async def setup(bot):
