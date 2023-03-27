11b14f11acbf431d04a16cd6969d561e129c6d4f
foxtrotyeah <foxtrottrick@gmail.com>
2023-03-27 17:35:30 -0600

    youtube-dl no longer works

--- TheBigGay/cogs/utils/audio.py
+++ TheBigGay/cogs/utils/audio.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import os
-import youtube_dl
 import asyncio
 
 
@@ -19,24 +18,14 @@ def _after_play(error: Exception):
         raise commands.CommandError(error)
 
 
-async def play(bot: commands.Bot, url: str, name: str, ctx: commands.Context = None, channel: discord.VoiceChannel = None, wait: int = 60 * 5): 
+async def play(client: discord.Client, name: str, ctx: commands.Context = None, channel: discord.VoiceChannel = None, wait: int = 60 * 5): 
     if ctx and not channel:
         channel = await _before_play(ctx)
 
     if not os.path.isfile(f"./tracks/{name}"):
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'outtmpl': f'./tracks/{name}',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192'
-            }],
-        }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
+        raise commands.CommandError(f"No track found for {name}")
 
-    voice = discord.utils.get(bot.voice_clients, guild=channel.guild)
+    voice = discord.utils.get(client.voice_clients, guild=channel.guild)
     if voice and voice.is_playing():
         raise commands.CommandError("I'm already playing something. Wait a sec")
 
