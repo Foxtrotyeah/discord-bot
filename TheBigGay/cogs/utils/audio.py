9149a35398aa7573b752553545776349bb326b38
foxtrotyeah <foxtrottrick@gmail.com>
2023-04-05 17:18:13 -0600

    play() now automatically disconnects after finishing

--- TheBigGay/cogs/utils/audio.py
+++ TheBigGay/cogs/utils/audio.py
@@ -1,37 +1,25 @@
 import discord
-from discord.ext import commands
-import os
+from discord import app_commands
 import asyncio
 
 
-async def _before_play(ctx: commands.Context) -> discord.VoiceChannel:
-    if ctx.author.voice:
-        channel = ctx.author.voice.channel
-    else:
-        channel = ctx.guild.voice_channels[0]
-
-    return channel
-
-    
-def _after_play(error: Exception):
-    if error:
-        raise commands.CommandError(error)
-
-
-async def play(client: discord.Client, name: str, ctx: commands.Context = None, channel: discord.VoiceChannel = None, wait: int = 60 * 5): 
-    if ctx and not channel:
-        channel = await _before_play(ctx)
-
-    if not os.path.isfile(f"./tracks/{name}"):
-        raise commands.CommandError(f"No track found for {name}")
-
+async def play(client: discord.Client, name: str, channel: discord.VoiceChannel = None, wait = False):
     voice = discord.utils.get(client.voice_clients, guild=channel.guild)
     if voice and voice.is_playing():
-        raise commands.CommandError("I'm already playing something. Wait a sec")
+        raise app_commands.AppCommandError("I'm already playing something. Wait a sec.")
 
     voice_client = await channel.connect()
 
-    voice_client.play(discord.FFmpegPCMAudio(source=f"./tracks/{name}"), after=_after_play)
+    def after_play(error: Exception):
+        asyncio.run_coroutine_threadsafe(voice_client.disconnect(), voice_client.loop).result()
 
-    await asyncio.sleep(wait)
-    await voice_client.disconnect()
+        nonlocal wait 
+        wait = False
+
+        if error:
+            raise app_commands.CommandInvokeError(error)
+
+    voice_client.play(discord.FFmpegPCMAudio(source=f"./tracks/{name}"), after=after_play)
+
+    while wait:
+        await asyncio.sleep(1)
