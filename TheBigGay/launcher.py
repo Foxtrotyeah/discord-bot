fdcc5ad89a9d06de3061c971389fc1594d2653f7
foxtrotyeah <foxtrottrick@gmail.com>
2023-12-09 23:51:15 -0700

    Created version variable

--- TheBigGay/launcher.py
+++ TheBigGay/launcher.py
@@ -1,10 +1,11 @@
 from bot import GayBot
 
 
-# The Big Gay version 3.0.1
-
-
 if __name__ == '__main__':
     bot = GayBot()
+
+    # The Big Gay version 3.0.1
+    bot.version = "3.0.1"
+
     bot.run()
     
